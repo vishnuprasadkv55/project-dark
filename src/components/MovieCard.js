5fd2274b9082d0175813fb0ce070e107ffb78b03
vishnu <vishnuprasadkv55@gmail.com>
2020-03-18 22:13:24 +0530

    adding components

---
+++ src/components/MovieCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MovieCard(props) {
+    return <div className="movie-card">
+        <div className="image-holder">
+    <img src={props.url} alt=""/>
+        </div>
+        <div className="name-holder">
+            jhgjh
+        </div>
+    </div>;  
+}
+
+export default MovieCard; 
