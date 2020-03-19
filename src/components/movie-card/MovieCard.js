1d1a8077a80e7058172c186a7fbdc7d6b4d89781
vishnu <vishnuprasadkv55@gmail.com>
2020-03-19 10:56:27 +0530

    updating elements

--- src/components/movie-card/MovieCard.js
+++ src/components/movie-card/MovieCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MovieCard(props) {
     return <div className="movie-card">
         <div className="image-holder">
-            <img src={props.url} alt="" onerror="this.onerror=null;this.src='imagefound.gif';"/>
+            <img src={props.url} alt=""/>
         </div>
         <div className="truncate text-gray-600">
             {props.name}
--- src/components/romantic-comedy/RomanticComedy.js
+++ src/components/romantic-comedy/RomanticComedy.js
@@ -3,16 +3,40 @@ import MovieCard from '../movie-card/MovieCard';
 import ContentPage1 from '../../assets/API/CONTENTLISTINGPAGE-PAGE1.json';
 import ContentPage2 from '../../assets/API/CONTENTLISTINGPAGE-PAGE2.json';
 import ContentPage3 from '../../assets/API/CONTENTLISTINGPAGE-PAGE3.json';
-class RomanticComedy extends React.Component {
 
-state = {
+class RomanticComedy extends React.Component {
+    constructor(props){
+    super(props)
+this.state = {
     movies: [],
+    moviesList: [],
     per: 2,
     page: 1,
     totalPages: 3,
-    scrolling: false
+    scrolling: false,
+    value: ''
+}
+    this.handleSearch = this.handleSearch.bind(this);
+    
+    const {createStore} = require('redux');
+this.store = createStore(myReducer);
+const myReducer = (state = this.state,action) =>{
+    
+    var updatedMoviesArray = [];
+    if(action.type === 'SEARCH') { 
+    updatedMoviesArray = this.state.moviesList.filter(function(item){
+        return item['name'].toLowerCase().includes(this.state.value)
+    });
+    this.setState({
+        movies : updatedMoviesArray
+    });
+    }
+}
+
 }
     
+
+
 componentWillMount() {
     this.loadMovies();
     this.scrollListner = window.addEventListener('scroll', (e) => {
@@ -32,10 +56,11 @@ handleScroll = (e) => {
     }
 }
 loadMovies(){
-    const { per, page, movies} = this.state
+    const { per, page, movies, moviesList} = this.state
     var loadedPage = (page === 1) ? ContentPage1 : (page === 2) ? ContentPage2 : ContentPage3;
     this.setState({
         movies : [...movies, ...loadedPage.page['content-items'].content],
+        moviesList: [...moviesList, ...loadedPage.page['content-items'].content],
         scrolling: false
     });
 }
@@ -46,14 +71,27 @@ loadMore() {
         scrolling: true
     }),this.loadMovies);
 }
+handleSearch(event) {
+
+    this.setState({value: event.target.value});
+    this.store.dispatch({type: 'SEARCH'});
+    // updatedMoviesArray = this.state.moviesList.filter(function(item){
+    //     return item['name'].toLowerCase().includes(event.target.value)
+    // });
+    // this.setState({
+    //     movies : updatedMoviesArray
+    // });
+  }
 render () {
     var imageUrl = 'https://raw.githubusercontent.com/vishnuprasadkv55/project-dark/master/src/assets/Slices/';
-    return (<div className="p-4 pt-20 bg-black grid gap-4 grid-cols-3 content-items">
-        {this.state.movies.map(function (item, index) {
-            var index = 0;
+    return (
+    <div className="pt-20">
+        <input  value={this.state.value} onChange={this.handleSearch} class="shadow bg-black appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline " id="username" type="text" placeholder="Search..."></input> 
+    <div className="p-4 bg-black grid gap-4 grid-cols-3 content-items">
+        {this.state.movies.map(function (item,index) {
             return (<MovieCard className="" key={index} name={item['name']} url={imageUrl + item['poster-image']}></MovieCard>);
 
-        })}</div>);
+        })}</div></div>);
 }
 }
 
